6c491fd788d2d945ce5a8dbbd3e54df4047fb613
Art-Xz <arturgaudie08@gmail.com>
2026-02-17 05:18:24 -0300

    beecrowd 1004 C and C++

---
+++ C++/somasimples.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int AA;
+    cin>>AA;
+    int BB;
+    cin>>BB;
+    int prod;
+    prod = AA*BB;
+    cout<<"PROD = "<<prod<<"\n";
+}
---
+++ C/prod.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int A;
+    scanf("%d",&A);
+    int B;
+    scanf("%d",&B);
+    int prod = A*B;
+    printf("PROD = %d\n",prod);
+
+    return 0;
+}
