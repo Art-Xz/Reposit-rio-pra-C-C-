92c9c2b5f1075f8fde9b6219b65e0ea9fc6a6602
Art-Xz <arturgaudie08@gmail.com>
2026-02-21 05:18:57 -0300

    beecrowd 1008 C

---
+++ C/salario.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int num,hour;
+    double sal,total;
+    scanf("%d %d %lf",&num,&hour,&sal);
+    total = hour*sal;
+    printf("NUMBER = %d\nSALARY =  U$ %.2f\n",num,total);
+}
