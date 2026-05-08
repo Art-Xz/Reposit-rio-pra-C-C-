bb6959ee36f41b271dda493ed4d979003f31fba2
Art-Xz <arturgaudie08@gmail.com>
2026-05-08 04:55:41 -0300

    beecrowd 1175 C

---
+++ C/trocavetor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int v[20];
+
+    for (int i = 19; i > -1; i--)
+    {
+        scanf("%d",&v[i]);
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        printf("N[%d] = %d\n",i,v[i]);
+    }
+    
+}
