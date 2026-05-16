824bd68fc8d20294a78c6d3fd765640446a0410a
Art-Xz <arturgaudie08@gmail.com>
2026-05-16 05:31:17 -0300

    beecrowd 1181 C++

---
+++ C++/linhamatriz.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double soma=0,media=0;
+    int val,v=12;
+    double m[v][v] = {0};
+    string ope;
+    cin>>val;
+    cin>>ope;
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            cin>>m[i][j];
+        }
+        
+    }
+    //soma------------
+    for (int i = 0; i < v; i++)
+    {
+        soma += m[val][i];
+    }
+    media = soma/v;
+
+    //-------------------
+    if(ope=="S"){
+        printf("%1.f\n",soma);
+    } else {
+        printf("%.1f\n",media);
+    }
+}
