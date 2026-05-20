c026129f43af5cc2e5f2d387e8881aba9bc621c3
Art-Xz <arturgaudie08@gmail.com>
2026-05-20 04:49:21 -0300

    beecrowd 1185 C++

---
+++ C++/acimadiagonal2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double soma=0,media=0;
+    int val,v=12;
+    double m[v][v] = {0};
+    string ope;
+    cin>>ope;
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            cin>>m[i][j];
+        }
+        
+    }
+    //soma------------
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            if(i+j < 11){
+                soma+=m[i][j];
+            }
+        }
+        
+    }
+    media = soma/66;
+
+    //-------------------
+    if(ope=="S"){
+        printf("%.1f\n",soma);
+    } else {
+        printf("%.1f\n",media);
+    }
+}
