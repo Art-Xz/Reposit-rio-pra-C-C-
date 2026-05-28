8698ea379e3d034b9369922896eea0a4c7f1e3b3
Art-Xz <arturgaudie08@gmail.com>
2026-05-28 05:26:18 -0300

    beecrowd 1534 C++

---
+++ C++/matriz123.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    while (cin>>n)
+    {
+        int m[n][n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if(j == n-1-i){
+                    m[i][j] = 2;
+                } else if(i==j) {
+                    m[i][j] = 1;
+                } else {m[i][j] = 3;}
+            }
+            
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout<<m[i][j];
+            }
+            cout<<"\n";
+        }
+        }
+    
+    
+
+}
