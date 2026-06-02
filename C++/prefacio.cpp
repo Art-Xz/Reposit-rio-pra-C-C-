5ca1d79bd3df71f82449d10df1acdb24b3f8973a
Art-Xz <arturgaudie08@gmail.com>
2026-06-02 05:15:34 -0300

    beecrowd 1837 C++

---
+++ C++/prefacio.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, q, r;
+    cin>>a>>b;
+    q = a/b;
+    r = a%b;
+    if(r<0){
+        if(b>0){
+            q--;
+            r +=b;
+        } else {
+            q++;
+            r -= b;
+        }
+    }
+    cout<<q<<" "<<r<<"\n";
+}
