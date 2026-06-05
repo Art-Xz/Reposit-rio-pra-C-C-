9be06f7b7a206ada369d1b404b52e2e6d05056d0
Art-Xz <arturgaudie08@gmail.com>
2026-06-05 06:06:02 -0300

    beecrowd 1864

---
+++ C/diasnaovoltaram.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char frase[] = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%c",frase[i]);
+    }
+    printf("\n");
+    
+
+}
